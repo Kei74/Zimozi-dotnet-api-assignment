b79cc18f56ac1f5f63430feed47d7fe5861bc445
kei74 <Kei74@ISERLOHN>
2025-05-16 15:48:37 +0530

    Added test project

---
+++ Zimozi-dotnet-api-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Zimozi_dotnet_api_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
