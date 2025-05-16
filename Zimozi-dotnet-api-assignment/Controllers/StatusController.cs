28ca8e4d4b6d2e92e49e9681dd21493c8554ac51
kei74 <Kei74@ISERLOHN>
2025-05-16 12:43:14 +0530

    Added status check endpoint

---
+++ Zimozi-dotnet-api-assignment/Controllers/StatusController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zimozi_dotnet_api_assignment.Controllers
+{
+    [ApiController]
+    [Route("/status")]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        public ActionResult Status()
+        {
+            return Ok("API running");
+        }
+    }
+}
