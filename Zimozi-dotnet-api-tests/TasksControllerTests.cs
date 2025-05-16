ba47da51ab45c10e31fdefb23f1173a7f1beb853
kei74 <Kei74@ISERLOHN>
2025-05-16 19:00:40 +0530

    Unit tests for UserTasksController

---
+++ Zimozi-dotnet-api-tests/TasksControllerTests.cs
@@ -0,0 +1,127 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Zimozi_dotnet_api_assignment.Controllers;
+using Zimozi_dotnet_api_assignment.data;
+using Zimozi_dotnet_api_assignment.Models.DTO;
+using Zimozi_dotnet_api_assignment.Models.Entities;
+using Zimozi_dotnet_api_assignment.Repositories;
+
+namespace Zimozi_dotnet_api_tests
+{
+    public class TasksControllerTests
+    {
+        private readonly UserTasksController _userTasksController;
+        private readonly Mock<IUserRepository> _usersMock = new Mock<IUserRepository>();
+        private readonly Mock<IUserTaskRepository> _tasksMock = new Mock<IUserTaskRepository>();
+
+        public TasksControllerTests()
+        {
+            _userTasksController = new UserTasksController(_usersMock.Object, _tasksMock.Object);
+        }
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _userTasksController.Dispose();
+        }
+
+        [Test]
+        public void GetAllTasks_ShouldReturnOk()
+        {
+
+            _tasksMock.Setup(x => x.GetAllWithUsers())
+                    .Returns(TestData.Tasks);
+
+            var result = _userTasksController.GetAllTasks();
+
+            Assert.That(result.GetType().Name, Is.EqualTo("OkObjectResult"));
+        }
+
+        [Test]
+        public void GetTaskById_ShouldReturnTask_WhenTaskExists()
+        {
+            UserTask task = TestData.Tasks[0];
+            TaskDto taskDto = new TaskDto
+            {
+                Id = task.Id,
+                Name = task.Name,
+                Description = task.Description,
+                AssignedUsername = task.AssignedUser.Username, 
+            };
+
+
+            _tasksMock.Setup(x => x.GetByIdWithUserAndComment(It.Is<Guid>(x => x == task.Id)))
+                    .Returns(task);
+
+            var result = _userTasksController.GetTaskById(task.Id);
+            Assert.That(result.GetType().Name, Is.EqualTo("OkObjectResult"));
+            var resultObject = (OkObjectResult)result;
+            Assert.That(resultObject.Value.ToString(), Is.EqualTo(taskDto.ToString()));
+        }
+
+        // Negative test case for get by Id
+        [Test]
+        public void GetTaskById_ShouldReturnNothing_WhenTaskDoesNotExists()
+        {
+            _tasksMock.Setup(x => x.GetByIdWithUserAndComment(It.IsAny<Guid>()))
+                    .Returns(() => null);
+
+            var result = _userTasksController.GetTaskById(new Guid());
+            Assert.That(result.GetType().Name, Is.EqualTo("NotFoundResult"));
+        }
+
+        [Test]
+        public void CreateTask_ShouldCreateTask_WhenInputValid()
+        {
+
+            UserTask task = TestData.Tasks[0];
+            AddTaskDto addTaskDto = new AddTaskDto
+            {
+                Description = task.Description,
+                Name = task.Name,
+                Username = task.AssignedUser.Username
+            };
+
+            TaskDto taskDto = new TaskDto
+            {
+                Id = task.Id,
+                Name = task.Name,
+                Description = task.Description,
+                AssignedUsername = task.AssignedUser.Username,
+            };
+
+            _usersMock.Setup(x => x.GetByUsername(It.Is<string>(x => x == task.AssignedUser.Username)))
+                    .Returns(() => task.AssignedUser);
+
+            var result = _userTasksController.CreateTask(addTaskDto);
+            Assert.That(result.GetType().Name, Is.EqualTo("CreatedAtActionResult"));
+            var resultObject = (CreatedAtActionResult)result;
+            Assert.That(resultObject.Value.ToString(), Is.EqualTo(taskDto.ToString()));
+        }
+
+        // Negative test case for create task
+        [Test]
+        public void CreateTask_ShouldFail_WhenUserNotValid()
+        {
+            UserTask task = TestData.Tasks[0];
+            AddTaskDto addTaskDto = new AddTaskDto
+            {
+                Description = task.Description,
+                Name = task.Name,
+                Username = task.AssignedUser.Username
+            };
+            _usersMock.Setup(x => x.GetByUsername(It.IsAny<string>()))
+                    .Returns(() => null);
+
+            var result = _userTasksController.CreateTask(addTaskDto);
+            Assert.That(result.GetType().Name, Is.EqualTo("BadRequestObjectResult"));
+        }
+    }
+}
---
+++ Zimozi-dotnet-api-tests/TestData.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zimozi_dotnet_api_assignment.Models.Entities;
+
+namespace Zimozi_dotnet_api_tests
+{
+    /// <summary>
+    /// Helper class to organize static test data
+    /// </summary>
+    static internal class TestData
+    {
+        private static readonly (string username, string password)[] AdminUserSeedList = new[]
+        {
+            ("Admin", "Adminpass"), ("Admin2", "Admin2pass"), ("Admin3", "Admin3pass")
+        };
+
+        private static readonly (string username, string password)[] UserSeedList = new[]
+        {
+            ("User", "Userpass"), ("User2", "User2pass"), ("User3", "User3pass")
+        };
+
+        private static readonly (string name, string description)[] TaskSeedList = new[]
+            {
+                ("Task 1", "Demo task"),
+                ("Task 2", "Demo task 2"),
+                ("Task 3", "Demo task 3"),
+                ("Task 4", "Demo task 4"),
+                ("Task 5", "Demo task 5"),
+                ("Task 6", "Demo task 6"),
+            };
+        public static List<User> Users = [];
+        public static List<UserTask> Tasks = [];
+        static TestData()
+        {
+            foreach (var userSeed in AdminUserSeedList)
+                Users.Add(CreateUser(userSeed, UserRole.Admin));
+            foreach (var userSeed in UserSeedList)
+                Users.Add(CreateUser(userSeed, UserRole.User));
+
+            Random r = new Random();
+            foreach (var taskSeed in TaskSeedList)
+            {
+                User randomUser = Users[r.Next(Users.Count)];
+                Tasks.Add(CreateTask(randomUser, taskSeed));
+            }
+
+
+        }
+
+        private static User CreateUser((string username, string password) userSeedData, UserRole role)
+        {
+            User user = new User
+            {
+                Username = userSeedData.username,
+                Password = userSeedData.password,
+                Role = role,
+            };
+            return user;
+        }
+
+        private static UserTask CreateTask(User user, (string name, string description) seedTaskData)
+        {
+            UserTask task = new UserTask
+            {
+                Name = seedTaskData.name,
+                Description = seedTaskData.description,
+                AssignedUser = user,
+                AssignedUserId = user.Id,
+            };
+            return task;
+        }
+    }
+}
+
